ec5dfc3aec5f957888e2deed55c20eb68b0864b4
DanyaYashin <yashindan2000@gmail.com>
2021-03-15 13:20:54 +0400

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import React from "react"
+import { Link } from "gatsby"
+import styles from "./index.module.css"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div className={styles.word}>Hello world!</div>
 }
